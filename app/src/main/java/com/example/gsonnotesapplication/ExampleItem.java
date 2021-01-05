fc5da1664be2debac93862ff825e0ccfd106f83f
herman <hermanjustino@gmail.com>
2021-01-05 14:30:48 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/gsonnotesapplication/ExampleItem.java
@@ -0,0 +1,19 @@
+package com.example.gsonnotesapplication;
+
+public class ExampleItem {
+    private String mLine1;
+    private String mLine2;
+
+    public ExampleItem(String line1, String line2) {
+        mLine1 = line1;
+        mLine2 = line2;
+    }
+
+    public String getLine1() {
+        return mLine1;
+    }
+
+    public String getLine2() {
+        return mLine2;
+    }
+}
